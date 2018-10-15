cb65a72ceefc2a7e1ffdf83a194db21bb8c99191
Oleh Medelyan <olezhyk88@gmail.com>
2018-10-15 18:02:44 +0300

    Create Enums: Country, Position

---
+++ WPFMentorshipProject/WPFMentorshipProject/EnumHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMentorshipProject.Properties;
+
+namespace WPFMentorshipProject
+{
+    public static class EnumHelper
+    {
+        public static string GetPlayerNationality(Country playerNationality)
+        {
+            switch (playerNationality)
+            {
+                case Country.Argentina:
+                    return Resources.ArgentineNationality;
+                default:
+                    return Resources.none;
+            }
+        }
+
+        public static string GetFederationName(Country federationName)
+        {
+            switch (federationName)
+            {
+                case Country.Argentina:
+                    return Resources.ArgentinaFederation;
+                default:
+                    return Resources.none;
+            }
+        }
+
+        public static string GetPleyerPosition(PlayerPosition playerPosition)
+        {
+            switch (playerPosition)
+            {
+                case PlayerPosition.Goalkeeper:
+                    return Resources.Goalkeeper;
+                default:
+                    return Resources.none;
+            }
+        }
+    }
+}
--- WPFMentorshipProject/WPFMentorshipProject/PlayerPosition.cs
+++ WPFMentorshipProject/WPFMentorshipProject/PlayerPosition.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace WPFMentorshipProject
 {
-    public class PlayerPosition
+    public enum PlayerPosition : byte
     {
-        enum Position : byte
-        {
-            Goalkeeper = 1,
-
-        }
+        Goalkeeper = 0,
+        LeftDefender = 1,
+        LeftCenterDefender = 2,
+        RightCenterDefender = 3,
+        RightDefender = 4,
+        LeftMiddleForward = 5,
+        LeftCenterMiddleForward = 6,
+        RightCenterMiddleForward = 7,
+        RightMiddleForward = 8,
+        LeftForward = 9,
+        RightForward = 10
     }
+
 }
